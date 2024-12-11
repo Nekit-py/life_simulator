1ddee80ed888ecfef34c1b09cceee931c70da082
sidorovich_ns <61461523+Nekit-py@users.noreply.github.com>
2024-12-11 16:42:39 +0700

    в комментах main пример который работает

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use rand::{rngs::ThreadRng, seq::SliceRandom};
 fn main() -> Result<(), std::io::Error> {
     let mut rng = thread_rng();
     // // let mut field = Field::new(50, 70);
-    let mut field = Field::new(2, 3);
+    let mut field = Field::new(3, 4);
     // let mut field = Field::new(1, 3);
     field.fill(&mut rng);
     println!("{}", field);
@@ -19,3 +19,30 @@ fn main() -> Result<(), std::io::Error> {
 
     Ok(())
 }
+
+////Работотающий пример
+// use std::fmt;
+
+// #[derive(Debug, Clone, Copy)]
+// struct Foo{
+//     val: i32
+// }
+
+// impl fmt::Display for Foo {
+//     fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+//       write!(f, "{}", self.val)?;
+//       Ok(())
+//     }
+// }
+
+// fn main() {
+//     let foo = Foo{val:1};
+//     let foo2 = Foo{val:2};
+//     let mut baz = vec![foo];
+//     println!("{:?}", baz);
+//     for v in baz.iter_mut() {
+//         *v = foo2;
+//     }
+//     println!("{:?}", baz);
+//     println!("{}", baz[0].val);
+// }
